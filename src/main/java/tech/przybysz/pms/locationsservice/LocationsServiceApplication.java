db738936eccd1267d741222473d35770ed35dd91
Andrzej <andrzej.przybysz01@gmail.com>
2021-03-11 10:02:37 +0100

    init

---
+++ src/main/java/tech/przybysz/pms/locationsservice/LocationsServiceApplication.java
@@ -0,0 +1,13 @@
+package tech.przybysz.pms.locationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationsServiceApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LocationsServiceApplication.class, args);
+  }
+
+}
---
+++ src/test/java/tech/przybysz/pms/locationsservice/LocationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.przybysz.pms.locationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationsServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
